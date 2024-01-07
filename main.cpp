c00be05562a2b6fe0b7e16355d3b29a43df7a675
sanha.hwang <sanhahwang.tech@gmail.com>
2024-01-07 21:10:30 +0900

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "src/SinglyLinkedList.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main(void) {
+    // SinglyLinkedList<double> list(0.1);
+    std::cout << "build complete" << std::endl;
+    // list.append(0.2);
+    // list.printList();
+    // list.append(0.3);
+    // list.printList();
+    // list.append(0.4);
+    // list.printList();
+
+    // list.insert(0.0, 0);
+    // list.printList();
+    // list.insert(2.0, 2);
+    // list.printList();
+    // list.insert(10.0, 10);
+    // list.printList();
+
+    // list.remove(0);
+    // list.printList();
+    // list.remove(5);
+    // list.printList();
+    // list.remove(2);
+    // list.printList();
+
+    return -1;
+}
