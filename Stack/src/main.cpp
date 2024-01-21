4d1aa8cb1976bba90b93ab8975f67769ff26b09b
sanha.hwang <sanhahwang.tech@gmail.com>
2024-01-22 00:34:16 +0900

    Test Stack class

--- Stack/src/main.cpp
+++ Stack/src/main.cpp
@@ -1,69 +1,63 @@
-#include "src/SinglyLinkedList.hpp"
-#include <iostream>
+#include "Stack.hpp"
 
 using namespace std;
 
 
 int main(void) {
-    SinglyLinkedList<double> list(0.1);
-    list.printList();
-    list.append(0.2);
-    list.printList();
-    list.append(0.3);
-    list.printList();
-    list.append(0.4);
-    list.printList();
-    std::cout << "\n======append DONE======\n\n" << std::endl;
+    Stack<double> stack(5);
+    stack.printStack();
+    stack.push(0.0);
+    stack.printStack();
+    stack.push(1.1);
+    stack.printStack();
+    stack.push(2.2);
+    stack.printStack();
+    stack.pop();
+    stack.printStack();
+    stack.push(3.3);
+    stack.printStack();
+    stack.push(4.4);
+    stack.printStack();
+    stack.push(5.5);
+    stack.printStack();
+    stack.push(6.6);
+    stack.printStack();
 
-    list.insert(-1.0, -1);
-    list.printList();
-    list.insert(0.0, 0);
-    list.printList();
-    list.insert(1.1, 1);
-    list.printList();
-    list.insert(2.2, 2);
-    list.printList();
-    list.insert(8.8, 8);
-    list.printList();
-    list.insert(7.7, 7);
-    list.printList();
-    list.insert(6.6, 6);
-    list.printList();
-    std::cout << "\n======insert DONE======\n\n" << std::endl;
+    stack.isFull();
+    stack.isEmpty();
+    std::cout << "\n======push DONE======\n\n" << std::endl;
+    double peak = stack.peak();
+    stack.pop();
+    std::cout << "popping peak: " << peak << std::endl;
+    stack.printStack();
+    peak = stack.peak();
+    stack.pop();
+    std::cout << "popping peak: " << peak << std::endl;
+    stack.printStack();
+    peak = stack.peak();
+    stack.pop();
+    std::cout << "popping peak: " << peak << std::endl;
+    stack.printStack();
+    peak = stack.peak();
+    stack.pop();
+    std::cout << "popping peak: " << peak << std::endl;
+    stack.printStack();
+    peak = stack.peak();
+    stack.pop();
+    std::cout << "popping peak: " << peak << std::endl;
+    stack.printStack();
+    peak = stack.peak();
+    stack.pop();
+    std::cout << "popping peak: " << peak << std::endl;
+    stack.printStack();
+    peak = stack.peak();
+    stack.pop();
+    std::cout << "popping peak: " << peak << std::endl;
+    stack.printStack();
 
-    list.remove(-1);
-    list.printList();
-    list.remove(0);
-    list.printList();
-    list.remove(5);
-    list.printList();
-    list.remove(6);
-    list.printList();
-    list.remove(100);
-    list.printList();
-    list.remove(5);
-    list.printList();
-    std::cout << "\n======remove DONE======\n\n" << std::endl;
-
-    list.remove(0);
-    list.printList();
-    list.remove(0);
-    list.printList();
-    list.remove(0);
-    list.printList();
-    list.remove(0);
-    list.printList();
-    list.remove(0);
-    list.printList();
-    list.remove(0);
-    list.printList();
-    list.remove(0);
-    list.printList();
-    list.remove(0);
-    list.printList();
-    list.remove(0);
-    list.printList();
-    std::cout << "\n======DONE======\n\n" << std::endl;
+    stack.isFull();
+    stack.isEmpty();
+    std::cout << "\n======pop DONE======\n\n" << std::endl;
 
     return -1;
 }
